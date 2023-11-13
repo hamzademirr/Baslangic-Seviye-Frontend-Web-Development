8dbd2d6594831cb7389a56642eb8a56297a7fb25
hamzademirr <demha082@gmail.com>
2023-11-13 23:47:47 +0300

    javascript derslerine baslandi

---
+++ js/degisken-tanımlama.js
@@ -0,0 +1,17 @@
+//============================ var ile degisken tanimlama ============================
+
+// var servername = "api.google.com"
+// console.log(servername)  //var ile degisken tanimlandiginda bos birakilir ise consolda "undefined" ile tanimlanir
+// ================ let ve const ile degisken tanimlama ====================
+
+//let servername;
+// console.log(servername)
+// let servername = "api.google.com" // bu sekilde kullanildiginda var da ki gibi "undefined"  uyari vermek yerine hata ile karsilasir
+
+// console.log(servername)
+// let servername = "api.google.com"  //dogru kullanimi
+
+// const servername = "api.google.com";
+// console.log(servername);
+// const servername = "api.facebook.com";
+// console.log(servername); // const sadece bir kez deger atana bilir ve degeri tanimlanir iken verilmelidir const degismeyecek veriler icin kullanilir
