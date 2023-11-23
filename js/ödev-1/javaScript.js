ede06cddf63c0fdeae47c021605ea40d5908054e
hamzademirr <demha082@gmail.com>
2023-11-23 04:11:10 +0300

    canlı tarih özelliği eklendi

--- js/ödev-1/javaScript.js
+++ js/ödev-1/javaScript.js
@@ -1,14 +1,25 @@
-let name = prompt("Adınızı girin")
+let name = prompt("Adınızı girin");
 
-let welcomText = document.querySelector("#welcome>strong")
-welcomText.innerHTML = `${name}`
+let welcomText = document.querySelector("#welcome>strong");
+welcomText.innerHTML = `${name}`;
 
-let dateNow = new Date();
-let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
-let date = document.querySelector("#date")
-let today = dateNow.getDay()
-date.innerHTML = ([
-    dateNow.getHours(),
-    dateNow.getMinutes(),
-    dateNow.getSeconds(),
-  ].join(":") + " " + days[today])
+setInterval(dateUpdate, 1000);
+
+function dateUpdate() {
+  let dateNow = new Date()
+  let days = [
+    "Pazar",
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+  ];
+  let today = dateNow.getDay();
+  let date = document.querySelector("#date");
+  date.innerHTML =
+    [dateNow.getHours(), dateNow.getMinutes(), dateNow.getSeconds()].join(":") +
+    " " +
+    days[today]
+}
